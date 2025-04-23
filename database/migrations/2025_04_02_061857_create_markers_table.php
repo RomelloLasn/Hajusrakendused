6646fe0b5ddd2fdaff489ed1ee8be774ffe02d3a
RomelloLasn <romello.lasn@ametikool.ee>
2025-04-23 08:23:34 +0200

    a

--- database/migrations/2025_04_02_061857_create_markers_table.php
+++ database/migrations/2025_04_02_061857_create_markers_table.php
@@ -11,7 +11,7 @@ class CreateMarkersTable extends Migration
             $table->id();
             $table->decimal('latitude', 10, 8);
             $table->decimal('longitude', 11, 8);
-            $table->string('description')->nullable();
+            
             $table->string('description')->nullable()->default('No description');
             $table->timestamps();
         });
